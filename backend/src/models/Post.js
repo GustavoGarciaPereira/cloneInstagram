dfad060b8243b728fd4dbee5f757ed7fd009932d
gustavo <gusgurtavo@gmail.com>
2019-06-17 04:33:03 -0300

    backend node.js frontend react.js

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const PostSchema=new mongoose.Schema({
     author: String,
-    plase: String,
+    place: String,
     description: String,
     hashtags: String,
     image: String,
